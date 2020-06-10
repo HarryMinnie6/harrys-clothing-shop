f0e1ee732e224393633cc0c157a557dcaea3447d
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-06-10 14:15:47 +0200

    added sign up form and styles etc

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 import Header from './components/header/header.component'
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
